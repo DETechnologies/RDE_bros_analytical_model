9a6771531a1b5223193a39ac471e4782296176d5
lrpalmer27 <lrpalmer@mun.ca>
2023-11-18 10:14:32 -0330

    add ability to do 2D plots, with a second y axis

--- Analytical_Model_Plotter.m
+++ Analytical_Model_Plotter.m
@@ -11,30 +11,93 @@ on=size(Output_dataNames);
 if o(2) ~= on(2)
     disp("Something wrong with size of data here; plz check")
 end 
-%% UI dropdown
-fig=uifigure("Name","Analytical Model Parameter Selector");
-f = uigridlayout(fig,[2 3]);
-f.RowHeight = {'3x','1x'};
-f.ColumnWidth = {'1x','1x','1x'};
+%% UI dropdown - 3d plot
+fig1=uifigure("Name","Analytical Model 3D Plotter");
+f1 = uigridlayout(fig1,[2 3]);
+f1.RowHeight = {'3x','2x'};
+f1.ColumnWidth = {'1x','1x','1x'};
 
-xval=uidropdown(f,"Items",[Output_dataNames(1,:)]);
+xval=uidropdown(f1,"Items",[Output_dataNames(1,:)]);
 
-yval=uidropdown(f,"Items",[Output_dataNames(1,:)]);
+yval=uidropdown(f1,"Items",[Output_dataNames(1,:)]);
 
-zval=uidropdown(f,"Items",[Output_dataNames(1,:)]);
+zval=uidropdown(f1,"Items",[Output_dataNames(1,:)]);
 
-b=uibutton(f,"Text","Plot","ButtonPushedFcn",@(src,event) plot(src,xval,yval,zval,Output_dataNames,Output));
+b=uibutton(f1,"Text","Plot","ButtonPushedFcn",@(src,event) plot3D(src,xval,yval,zval,Output_dataNames,Output));
 b.Layout.Row = 2;
 b.Layout.Column = 2;
 
-%% main plot
+%% UI dropdown - 2D plot
+fig2=uifigure("Name","Analytical Model 2D Plotter");
+f2 = uigridlayout(fig2,[3 2]);
+f3.RowHeight = {'3x','2x'};
+f3.ColumnWidth = {'1x','1x'};
+
+xval=uidropdown(f2,"Items",[Output_dataNames(1,:)]);
+
+yval=uidropdown(f2,"Items",[Output_dataNames(1,:)]);
+
+cb=uicheckbox(f2,"Text","Two y-axis?");
+yval2=uidropdown(f2,"Items",[Output_dataNames(1,:)]);
+
+b=uibutton(f2,"Text","Plot","ButtonPushedFcn",@(src,event) plot2D(src,xval,yval,yval2,Output_dataNames,Output,cb));
+b.Layout.Row = 3;
+b.Layout.Column = 2;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+%% plot functions;
 %plot these variable : 
-function plot(src,xval,yval,zval,Output_dataNames,Output)
+function plot2D(src,xval,yval,yyvalueRight,Output_dataNames,Output,secondAx)
+    xIndex = find(contains(Output_dataNames(1,:),xval.Value));
+    yIndex = find(contains(Output_dataNames(1,:),yval.Value));
+
+    figure("Name","Analytical Model 2D Plot O/P")
+    scatter(Output(:,xIndex),Output(:,yIndex));
+    title("Analytical Model O/P")
+
+    xlabel(xval.Value)
+    ylabel(yval.Value)
+
+    if secondAx.Value == 1
+        yyindexRight = find(contains(Output_dataNames(1,:),yyvalueRight.Value));
+        yyright=Output(:,yIndex);
+        xxval=Output(:,xIndex);
+        addAxis2Plot(xxval,yyright);
+        ylabel(yyvalueRight.Value)
+    end 
+ 
+end 
+
+function addAxis2Plot(xxval,yyright)
+    yyaxis right 
+    scatter(xxval,yyright);
+end 
+
+
+%plot these variable : 
+function plot3D(src,xval,yval,zval,Output_dataNames,Output)
     xIndex = find(contains(Output_dataNames(1,:),xval.Value));
     yIndex = find(contains(Output_dataNames(1,:),yval.Value));
     zIndex = find(contains(Output_dataNames(1,:),zval.Value));
-
-    figure("Name","Analytical Model O/P")
+shak 
+    figure("Name","Analytical Model 3D Plot O/P")
     scatter3(Output(:,xIndex),Output(:,yIndex),Output(:,zIndex));
     title("Analytical Model O/P")
 
@@ -42,4 +105,5 @@ function plot(src,xval,yval,zval,Output_dataNames,Output)
     ylabel(yval.Value)
     zlabel(zval.Value)
 end 
+
 
